bba84abc644687abb04f475b4dfd7b53b98ccd6b
Syndesmos22 <sebastianisantiago14@gmail.com>
2025-09-26 10:27:23 +0200

    AAAAAAAAAAAAAAAAAAAA

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,18 @@ package org.example;
  */
 public class App 
 {
+    String[] ticket = new String[];
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+    private void addproduct (String nombre, String id, Categoria categoria, float precio){
+
+    }
+    private void prodlist{
+
+
+}
+
+
 }
