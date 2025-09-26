b9d8b4a332a7c1b53bc7743110264e4a2908f19f
Gunji Erkhembayar <gunjinlkham.e@alumnos.upm.es>
2025-09-26 10:25:15 +0200

    gg

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Welcome to the ticket module App.\n" +
+                "Ticket module. Type 'help' to see commands.");
+    }
+
+
+    private void help (){
+        System.out.println("Commands:");
+        System.out.println(" prod add <id> \"<name>\" <category> <price>");
+        System.out.println(" prod list");
+        System.out.println(" prod update <id> NAME|CATEGORY|PRICE <value>");
+        System.out.println(" prod remove <id>");
+        System.out.println(" ticket new");
+        System.out.println(" ticket add <prodId> <quantity>");
+        System.out.println(" ticket remove <prodId>");
+        System.out.println(" ticket print");
+        System.out.println(" echo \"<texto>\"");
+        System.out.println(" help");
+        System.out.println(" exit");
+        System.out.println("Categories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS");
+        System.out.println("Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOK 10%,\n" +
+                "ELECTRONICS 3%.");
     }
 }
