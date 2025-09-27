2a85682c969864c42c728b236d914c4c92b0b242
Syndesmos22 <sebastianisantiago14@gmail.com>
2025-09-27 20:31:44 +0200

    Le agregué límites a los getters y setters (que el nombre deba ser no nulo, etc)

--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -16,27 +16,44 @@ public class Producto {
     public Integer getIdentificador() {
         return identificador;
     }
+
     public void setIdentificador(Integer identificador) {
+        if(identificador == null || identificador <=0){
+            throw new IllegalArgumentException("El identificador no puede estar vacío y debe ser un número positivo");
+        }
         this.identificador = identificador;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
+        if(nombre== null || nombre.isBlank() || nombre.length() >=100){
+            throw new IllegalArgumentException("El nombre no puede estar vacío y debe contener menos de 100 caracteres");
+        }
         this.nombre = nombre;
     }
 
     public Categoria getCategoria() {
         return categoria;
     }
+
     public void setCategoria(Categoria categoria) {
+        if(categoria==null){
+            throw new IllegalArgumentException("La categoría no puede ser nula");
+        }
         this.categoria = categoria;
     }
+
     public float getPrecio() {
         return precio;
     }
 
     public void setPrecio(float precio) {
+        if(precio<=0){
+            throw new IllegalArgumentException("El precio debe ser un número mayor a 0 sin límite superior");
+        }
         this.precio = precio;
     }
 }
