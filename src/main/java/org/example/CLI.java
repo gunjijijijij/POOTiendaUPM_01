9ff68f5d1ff144ef2241034b52c0b02c8dea6d8f
Syndesmos22 <sebastianisantiago14@gmail.com>
2025-10-01 21:19:23 +0200

    No he borrado nada, solo agregué los métodos addproduct y prodlist

--- src/main/java/org/example/CLI.java
+++ src/main/java/org/example/CLI.java
@@ -3,8 +3,13 @@ package org.example;
 import java.util.Scanner;
 
 public class CLI {
-    public CLI(){
+    private static final int MAX_PRODUCTS = 200;
+    private Product[] products;
+    private int productcount;
 
+    public CLI(){
+        this.products = new Product[MAX_PRODUCTS];
+        this.productcount = 0;
     }
 
     public void start() {
@@ -24,7 +29,7 @@ public class CLI {
                     break;
                 case "echo":
                     if (commandUni.length < 2)
-                        System.err.println("echo comand need two parameters \"\"echo \"<text>\" \"\"");
+                        System.err.println("echo command needs two parameters \"\"echo \"<text>\" \"\"");
 
                     for (int i = 1; i < commandUni.length; i++) {
                         commandUni[i-1] = commandUni[i];
@@ -80,7 +85,7 @@ public class CLI {
 
     private void handleprodcommand(String[] args){
         if (args.length < 2) {
-            System.err.println("prod comand need two parameters \"\"prod \"<add|list|update|remove> ...\" \"\"");
+            System.err.println("prod command needs two parameters \"\"prod \"<add|list|update|remove> ...\" \"\"");
             return;
         }
 
@@ -98,7 +103,7 @@ public class CLI {
 
     private void handleticketcommand(String[] args){
         if (args.length < 2) {
-            System.err.println("prod comand need two parameters \"\"prod \"<add|list|update|remove> ...\" \"\"");
+            System.err.println("prod command needs two parameters \"\"prod \"<add|list|update|remove> ...\" \"\"");
             return;
         }
 
@@ -114,12 +119,32 @@ public class CLI {
         }
     }
 
-    /*
-    private void addproduct (String nombre, String id, Categoria categoria, float precio){
+    private void addproduct (String nombre, int id, Categoria categoria, float precio){
+        for(int i=0; i<productcount; i++){
+            if(products[i].getId().equals(id)){ //comprobamos que no existan dos productos con el mismo id
+                throw new IllegalArgumentException("Existen dos productos con el mismo id");
+            }
+        }
+        if(productcount >= MAX_PRODUCTS){ //verificamos el límite de productos
+            throw new IllegalStateException("No se pueden agregar más productos, el máximo es " + MAX_PRODUCTS);
+        }
+        Product product = new Product(id, nombre, categoria,precio); //creamos y agregamos el producto
+        products[productcount] = product;
+        productcount++; //aumentamos el contador
 
     }
     private void prodlist(String[] listaproductos){
+        System.out.println("Catalog:");
+        if(productcount == 0){
+            System.out.println("(empty)"); //No sé si hay que poner algo cuando esté vacío (?)
+        }else{
+            for(int i=0; i<productcount;i++){
+                Product product = products[i];
+                System.out.println("(class:Product, id:" + product.getId() + ", name: '" + product.getName() + "', category:" + product.getCategory() + ", price: " + product.getPrice() + ")");
+            }
+        }
+        System.out.println("prod list: ok");
+
 
     }
-     */
 }
