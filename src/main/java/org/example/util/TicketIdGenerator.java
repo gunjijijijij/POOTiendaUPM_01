794a2a945252458eb960a8134b4a0f28775e5ca2
Javier de la Cruz Sánchez <javier023javi@gmail.com>
2025-11-17 12:32:46 +0100

    Create new generateOpenTicketId and generateCloseTicketId

--- src/main/java/org/example/util/TicketIdGenerator.java
+++ src/main/java/org/example/util/TicketIdGenerator.java
@@ -4,5 +4,29 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class TicketIdGenerator {
+    private static String generateOpenTicketId(){
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd-HH:mm-");
+
+        String fechaFormateada = ahora.format(formatter);
+
+        return fechaFormateada;
+    }
+
+    private static String generateCloseTicketId(){
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("-yy-MM-dd-HH:mm");
+
+        String fechaFormateada = ahora.format(formatter);
+
+        return fechaFormateada;
+    }
+
+
+
 }
