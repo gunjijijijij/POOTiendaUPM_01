e706da31919a483792ac2e2e54b6151744d313e2
Gunji Erkhembayar <gunjinlkham.e@alumnos.upm.es>
2025-11-18 15:05:45 +0100

    porductos personalizables

--- src/main/java/org/example/CustomProduct.java
+++ src/main/java/org/example/CustomProduct.java
@@ -4,21 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomProduct extends Product{
-    private int maxCustomtexts;
     private List<String> customtexts;
-    public CustomProduct(int id, String name, Category category, float price, int maxCustomtexts){
-        super(id,name,category,price);
-        this.maxCustomtexts = maxCustomtexts;
+    public CustomProduct(int id, String name, Category category, float price, Integer maxPers){
+        super(id,name,category,price,maxPers);
         this.customtexts = new ArrayList<>();
     }
     public void addCustomText(String text){
-        if(customtexts.size() >= maxCustomtexts){
-            throw new IllegalArgumentException("Maximum custom texts reached: " + maxCustomtexts);
+        if(customtexts.size() >= maxPers){
+            throw new IllegalArgumentException("Maximum custom texts reached: " + maxPers);
         }
         customtexts.add(text);
+        calculatePrice();
     }
+    private void calculatePrice() {
+        this.price = super.price + (super.price * (float) 0.10 * customtexts.size());
+    }
+
     public List<String> getCustomtexts() {return new ArrayList<>(customtexts);}
-    public int getMaxCustomtexts(){return maxCustomtexts;}
+
+    public int getMaxCustomtexts(){return maxPers;}
+
     @Override
     public float getPrice(){
         return super.getPrice() * (1 + (0.10f * customtexts.size()));
@@ -33,7 +38,7 @@ public class CustomProduct extends Product{
                 + ", category: " + category
                 + ", base price: " + super.getPrice()
                 + ", final price: " + getPrice()
-                + ", maxCustomtexts: " + maxCustomtexts + "}";
+                + ", maxCustomtexts: " + maxPers + "}";
     }
     public void clearCustomTexts(){
         customtexts.clear();
