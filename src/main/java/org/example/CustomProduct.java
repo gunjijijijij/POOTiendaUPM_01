0aa7440f4cb593b35e90470b8ae707204d6b167a
CarrotAsh <12blurryface12@gmail.com>
2025-11-26 12:39:44 +0100

    formated

--- src/main/java/org/example/CustomProduct.java
+++ src/main/java/org/example/CustomProduct.java
@@ -3,37 +3,46 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomProduct extends Product{
+public class CustomProduct extends Product {
     private List<String> customtexts;
-    public CustomProduct(int id, String name, Category category, float price, Integer maxPers){
-        super(id,name,category,price,maxPers);
+
+    public CustomProduct(int id, String name, Category category, float price, Integer maxPers) {
+        super(id, name, category, price, maxPers);
         this.customtexts = new ArrayList<>();
     }
-    public void addCustomText(String text){
 
-        if(customtexts.size() >= maxPers){
+    public void addCustomText(String text) {
+
+        if (customtexts.size() >= maxPers) {
             throw new IllegalArgumentException("Maximum custom texts reached: " + maxPers);
         }
         customtexts.add(text.trim());
         calculatePrice();
     }
+
     private void calculatePrice() {
         this.price = super.price + (super.price * (float) 0.10 * customtexts.size());
     }
 
-    public List<String> getCustomtexts() {return new ArrayList<>(customtexts);}
+    public List<String> getCustomtexts() {
+        return new ArrayList<>(customtexts);
+    }
 
-    public int getMaxCustomtexts(){return maxPers;}
+    public int getMaxCustomtexts() {
+        return maxPers;
+    }
 
     @Override
-    public float getPrice(){
+    public float getPrice() {
         return super.getPrice() * (1 + (0.10f * customtexts.size()));
     }
-    public String getType(){
+
+    public String getType() {
         return "CustomProduct";
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "{class: Product, id: " + id
                 + ", name: '" + name
                 + ", category: " + category
@@ -41,7 +50,8 @@ public class CustomProduct extends Product{
                 + ", final price: " + getPrice()
                 + ", maxCustomtexts: " + maxPers + "}";
     }
-    public void clearCustomTexts(){
+
+    public void clearCustomTexts() {
         customtexts.clear();
     }
 }
--- src/main/java/org/example/FoodProduct.java
+++ src/main/java/org/example/FoodProduct.java
@@ -3,21 +3,21 @@ package org.example;
 import java.time.LocalDate;
 
 public class FoodProduct extends Product {
-    private LocalDate expirationdate;
-    private int maxpeople;
-    private float priceperperson;
+    private LocalDate expirationDate;
+    private int maxPeople;
+    private float pricePerPerson;
 
-    public FoodProduct(int id, String name, float priceperperson, LocalDate expirationdate, int maxpeople) {
-        super(id, name, null, 0, maxpeople);
-        if (maxpeople > 100 || maxpeople <= 0) {
+    public FoodProduct(int id, String name, float pricePerPerson, LocalDate expirationDate, int maxPeople) {
+        super(id, name, null, 0, maxPeople);
+        if (maxPeople > 100 || maxPeople <= 0) {
             throw new IllegalArgumentException("Max people MUST be between 1 and 100");
         }
-        if (!isValidCreation(expirationdate)) {
+        if (!isValidCreation(expirationDate)) {
             throw new IllegalArgumentException("Food product requires AT LEAST 3 days planning");
         }
-        this.expirationdate = expirationdate;
-        this.maxpeople = maxpeople;
-        this.priceperperson = priceperperson;
+        this.expirationDate = expirationDate;
+        this.maxPeople = maxPeople;
+        this.pricePerPerson = pricePerPerson;
     }
 
     //TODO ESTE METODO SOLO COMPRUEBA QUE DENTRO DE 3 DIAS NO LLEGUE A LA FECHA DE CADUCIDAD, NO SE ME OCURRIÓ COMO COMPROBAR QUE LA PLANIFICACION SEA >=3 DIAS
@@ -26,11 +26,11 @@ public class FoodProduct extends Product {
     }
 
     public LocalDate getExpirationdate() {
-        return expirationdate;
+        return expirationDate;
     }
 
     public int getMaxpeople() {
-        return maxpeople;
+        return maxPeople;
     }
 
     public String getType() {
@@ -41,15 +41,15 @@ public class FoodProduct extends Product {
     public String toString() {
         return "{class: Product, id: " + id
                 + ", name: '" + name
-                + ", price per person: " + priceperperson
-                + ", expirationdate: " + expirationdate
-                + ", maxpeople: " + maxpeople + "}";
+                + ", price per person: " + pricePerPerson
+                + ", expirationdate: " + expirationDate
+                + ", maxpeople: " + maxPeople + "}";
     }
 
     public float calculateTotalPrice(int numberofpeople) {
-        if (numberofpeople > maxpeople || numberofpeople <= 0) {
-            throw new IllegalArgumentException("Number of people must be between 1 and " + maxpeople);
+        if (numberofpeople > maxPeople || numberofpeople <= 0) {
+            throw new IllegalArgumentException("Number of people must be between 1 and " + maxPeople);
         }
-        return priceperperson * numberofpeople;
+        return pricePerPerson * numberofpeople;
     }
 }
