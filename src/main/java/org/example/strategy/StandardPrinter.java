f58bcb74a38f487f00cc67e80585ee92b2a479d8
Syndesmos22 <sebastianisantiago14@gmail.com>
2026-01-17 15:29:14 +0100

    Cambio en StandardPrinter para que maneje Common y Company

--- src/main/java/org/example/strategy/StandardPrinter.java
+++ src/main/java/org/example/strategy/StandardPrinter.java
@@ -1,21 +1,27 @@
 package org.example.strategy;
+import org.example.*;
 
-import org.example.CommonTicket;
-import org.example.Product;
-import org.example.Ticket;
 import java.util.List;
 
 public class StandardPrinter implements ITicketPrinter { //ESTRATEGIA DE IMPRIMIR TICKET NORMAL (MISMA DE LA ENTREGA ANTERIOR)
     @Override
     public void print(Ticket ticket) {
-        CommonTicket commonTicket = (CommonTicket) ticket;
         System.out.println("--- TICKET ESTÁNDAR ---");
-        List<Product> lines = commonTicket.getProducts();
+        List<Product> lines;
+        if (ticket instanceof CommonTicket) {             //verificamos el tipo de ticket
+            lines = ((CommonTicket) ticket).getProducts();
+        } else if (ticket instanceof CompanyTicket) {
+            lines = ((CompanyTicket) ticket).getProducts();
+        } else {
+            System.out.println("Error: Tipo de ticket irreconocible para impresión estándar.");
+            return;
+        }
         double total = 0;
         double totalDiscount = 0;
 
         for (Product p : lines) {
-            long count = lines.stream().filter(x -> x.getCategory() == p.getCategory()).count();
+            Category currentCategory = p.getCategory();
+            long count = lines.stream().filter(x -> x.getCategory() == currentCategory).count();
             double discount = (count > 1) ? p.getDiscount() : 0;
             System.out.printf("%-20s %.2f (Dto: %.2f)\n", p.getName(), p.getPrice(), discount);
             total += p.getPrice();
